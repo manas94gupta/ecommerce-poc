23fdb3fb1d689d4ca511060464e2c2dae35899c9
Manas <manas94gupta@gmail.com>
2018-05-29 04:23:34 +0530

    fetch and show product listings

--- src/components/ProductListings/ListingItem/index.js
+++ src/components/ProductListings/ListingItem/index.js
@@ -4,17 +4,13 @@ import { Link } from "react-router-dom";
 // Css
 import "./ListingItem.css";
 
-const ListingItem = () => (
+const ListingItem = ({ imgUrl, title, price }) => (
     <Link to="/" className="listingitem">
         <div className="listingitem__img-wrapper">
-            <img
-                className="listingitem__img"
-                src="https://assignment-appstreet.herokuapp.com/iphone_32gb_silver.jpg"
-                alt=""
-            />
+            <img className="listingitem__img" src={imgUrl} alt="" />
         </div>
-        <div className="listingitem__title">Product Name</div>
-        <div className="listingitem__price">₹ 4324</div>
+        <div className="listingitem__title">{title}</div>
+        <div className="listingitem__price">₹ {price.toFixed(2)}</div>
     </Link>
 );
 
--- src/components/ProductListings/index.js
+++ src/components/ProductListings/index.js
@@ -7,10 +7,56 @@ import ListingItem from "./ListingItem";
 import "./ProductListings.css";
 
 class ProductListings extends Component {
+    state = {
+        products: [],
+    };
+    componentDidMount() {
+        fetch(`${process.env.REACT_APP_API_URL}/products?page=1`, {
+            method: "GET",
+        })
+            .then(response => {
+                if (response.status !== 200) {
+                    // toast.error("Looks like there was a problem. Try Again!");
+                    // this.setState({
+                    //     loading: false,
+                    // });
+                    return;
+                }
+
+                // Examine the text in the response
+                response.json().then(data => {
+                    if (data.success) {
+                        this.setState({
+                            products: data.products,
+                        });
+                    }
+                    // this.setState({
+                    //     details: { ...data },
+                    //     loading: false,
+                    // });
+                    return;
+                });
+            })
+            .catch(err => {
+                // console.log("Cannot retrieve Data:", err);
+                // toast.error("Something Went Wrong. Try Again Later!");
+                // this.setState({
+                //     loading: false,
+                // });
+            });
+    }
+
     render() {
         return (
             <div className="productlisting flex-row">
-                <ListingItem />
+                {this.state.products.map((product, index) => (
+                    <ListingItem
+                        key={index}
+                        imgUrl={product.images[0]}
+                        title={product.name}
+                        price={product.sale_price}
+                    />
+                ))}
             </div>
         );
     }
